de7a0131f8baa02dd574b9711758574bab01c60f
Henrik Karlsson <hk.henrik@gmail.com>
2015-06-16 13:21:04 +0200

    Livereload

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 var Metalsmith = require('metalsmith'),
     markdown = require('metalsmith-markdown'),
     permalinks = require('metalsmith-permalinks'),
-    templates = require('metalsmith-templates');
+    templates = require('metalsmith-templates'),
+    watch = require('metalsmith-watch'),
+    define = require('metalsmith-define');
 
-Metalsmith(__dirname)
+
+var watchEnabled = process.argv.length > 2 && process.argv[2] === 'watch';
+
+var metalsmith = Metalsmith(__dirname)
     .use(markdown())
     .use(permalinks({
         pattern: ':title'
     }))
     .use(templates('jade'))
-    .build(function (err) {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log('Blog built');
-        }
-    });
+    .use(define({
+        watch: watchEnabled
+    }));
+
+
+if (watchEnabled) {
+    metalsmith.use(
+        watch({
+            paths: {
+                "${source}/**/*": true,
+                "templates/**/*": "**/*"
+            },
+            livereload: true
+        })
+    );
+}
+
+
+metalsmith.build(function (err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Blog built');
+    }
+});
